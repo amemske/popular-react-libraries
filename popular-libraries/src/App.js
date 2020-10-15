e315847cad8ac7582a38c2b26399e7c40902bf04
Antony Memia <antony@olywave.com>
2020-10-15 23:41:36 +0300

    Boostrap widgets and buttons

--- popular-libraries/src/App.js
+++ popular-libraries/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {IconContext} from 'react-icons';
 import { FaBeer } from 'react-icons/fa';
 import { BsFillArchiveFill } from 'react-icons/bs';
 import {toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Modal, Button } from 'react-bootstrap';
 
 const CustomToast = ({closeToast}) =>{
   return(
@@ -14,6 +16,11 @@ const CustomToast = ({closeToast}) =>{
 
 toast.configure()
 function App() {
+
+  const [showModal, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const notify = () =>{
     toast('Basic notification', {position: toast.POSITION.BOTTOM_CENTER, autoClose: 8000})
@@ -30,6 +37,31 @@ function App() {
           <BsFillArchiveFill color="blue" size= '10rem'/>
      </IconContext.Provider>
      <button onClick={notify}>Notify!</button>
+     <Button variant="primary">Click Me!</Button>
+
+     <div
+        className="d-flex align-items-center justify-content-center"
+        style={{ height: "100vh" }}
+      >
+        <Button variant="primary" onClick={handleShow}>
+          Launch demo modal
+        </Button>
+      </div>
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+     
     </div>
   );
 }
